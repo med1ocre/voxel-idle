a0a86cc6155aa606b5ba535b1924db9bf0f8b140
Jonathan Loftus <jonathan.loftus@mrisoftware.com>
2023-09-13 16:30:45 -0400

    EOD push

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import goldcoin from '../assets/media/icons/goldcoin.png';
 import bankchest from '../assets/media/icons/bankchest.png';
-
+import attack from '../assets/media/skills/attack/attack.png';
+import strength from '../assets/media/skills/strength/strength.png';
+import defence from '../assets/media/skills/defence/defence.png';
+import hitpoints from '../assets/media/skills/hitpoints/hitpoints.png';
+import settings from '../assets/media/icons/settings.png';
+import completion from '../assets/media/icons/completion.png';
+import statistics from '../assets/media/icons/statistics.png';
+import leaderboard from '../assets/media/icons/leaderboard.png';
 
 const Sidebar = ({ onSelectContent }) => {
   return (
@@ -31,7 +38,7 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/skills/attack/attack.svg" style={{ width: '1.5rem'}} alt="Attack Icon" />
+                <img src={attack} style={{ width: '1.5rem'}} alt="Attack Icon" />
                 <span className="sidebar-item-title">Attack</span>
                 </div>
                 <span style={{ marginLeft: 'auto' }}>(1 / 99)</span>
@@ -40,7 +47,7 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/skills/strength/strength.svg" style={{ width: '1.5rem'}} alt="Strength Icon" />
+                <img src={strength} style={{ width: '1.5rem'}} alt="Strength Icon" />
                 <span className="sidebar-item-title">Strength</span>
                 </div>
                 <span style={{ marginLeft: 'auto' }}>(1 / 99)</span>
@@ -49,7 +56,7 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/skills/defence/defence.svg" style={{ width: '1.5rem'}} alt="Strength Icon" />
+                <img src={defence} style={{ width: '1.5rem'}} alt="Strength Icon" />
                 <span className="sidebar-item-title">Defence</span>
                 </div>
                 <span style={{ marginLeft: 'auto' }}>(1 / 99)</span>
@@ -58,7 +65,7 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/skills/hitpoints/hitpoints.svg" style={{ width: '1.5rem'}} alt="Strength Icon" />
+                <img src={hitpoints} style={{ width: '1.5rem'}} alt="Strength Icon" />
                 <span className="sidebar-item-title">Hitpoints</span>
                 </div>
                 <span style={{ marginLeft: 'auto' }}>(1 / 99)</span>
@@ -88,17 +95,17 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/main/question.svg" style={{ width: '1.5rem'}} alt="Attack Icon" />
+                <img src={completion} style={{ width: '1.5rem'}} alt="Attack Icon" />
                 <span className="sidebar-item-title">Completion Log</span>
                 </div>
-                
+                <span style={{ marginLeft: 'auto' }}>(0.00%)</span>
             </a>
         </li>
 
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/main/question.svg" style={{ width: '1.5rem'}} alt="Attack Icon" />
+                <img src={statistics} style={{ width: '1.5rem'}} alt="Attack Icon" />
                 <span className="sidebar-item-title">Statistics</span>
                 </div>
             </a>
@@ -116,7 +123,7 @@ const Sidebar = ({ onSelectContent }) => {
         <li>
             <a href="#" className="nav-link link-light" style={{ display: 'flex', alignItems: 'center' }} onClick={() => onSelectContent('combat')}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                <img src="https://cdn.melvor.net/core/v018/assets/media/main/question.svg" style={{ width: '1.5rem'}} alt="Attack Icon" />
+                <img src={settings} style={{ width: '1.5rem'}} alt="Attack Icon" />
                 <span className="sidebar-item-title">Settings</span>
                 </div>
             </a>
